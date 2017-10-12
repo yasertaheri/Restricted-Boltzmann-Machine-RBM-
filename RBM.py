aee71f90c551daf8a261afd4eebcb87b33d51b17
yasertaheri <yasermt62@gmail.com>
2017-10-12 13:44:12 -0400

    first commit

---
+++ RBM.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 14:17:52 2017
+
+@author: y_moh
+"""
+import tensorflow as tf
+import numpy as np
+
+
+class RBM:
+    
+      def __init__(self,visible_size, hidden_size, inp, learning_rate):
+          self.visible_size = visible_size
+          self.hidden_size = hidden_size
+          self.Xt = inp
+          self.learning_rate = learning_rate
+        
+          with tf.variable_scope('layer'):
+
+               self.W = tf.get_variable(name="W", shape= [self.visible_size, self.hidden_size], initializer = tf.random_normal_initializer(stddev=0.01)) #The weight matrix that stores the edge weights
+               self.b = tf.get_variable(name= "hidde_bias", shape=[1,self.hidden_size], initializer = tf.zeros_initializer(), )                     #The bias vector for the hidden layer
+               self.c = tf.get_variable(name= "input_bias", shape=[1,self.visible_size], initializer = tf.zeros_initializer()) 
+
+          
+          def sample(p):
+              
+              prop = tf.random_uniform(tf.shape(p), 0,1)
+              return tf.floor(prop + p) 
+         
+          self.h_prop = tf.nn.sigmoid(tf.matmul(self.Xt , self.W) + self.b)
+          self.h_k = sample(self.h_prop)
+
+          self.x_prop = tf.nn.sigmoid(tf.matmul(self.h_k , tf.transpose(self.W)) + self.c)
+          self.x_k = sample(self.x_prop)
+          
+          self.hs_prop = tf.nn.sigmoid(tf.matmul(self.x_k , self.W) + self.b)
+          self.hs_k = sample(self.hs_prop)
+          
+          
+          self.W_del =  learning_rate * (1/10)*tf.subtract(tf.matmul(tf.transpose(self.Xt), self.h_k) , tf.matmul(tf.transpose(self.x_k), self.hs_k) )
+          self.b_del =  learning_rate * (1/10)*tf.reduce_sum((tf.subtract(self.h_k , self.hs_k)), 0, keep_dims=True)
+          self.c_del =  learning_rate * (1/10)*tf.reduce_sum(tf.subtract(self.Xt , self.x_k), 0, keep_dims=True)
+          
+          self.W_up = self.W.assign_add(self.W_del)
+          self.b_up = self.b.assign_add(self.b_del)
+          self.c_up = self.c.assign_add(self.c_del)
+          
---
+++ RBM_main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 15:19:43 2017
+
+@author: y_moh
+"""
+
+from RBM import RBM
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+num_epochs = 20000
+Batch_size = 10
+input_size = 28*28 
+hidden_size = 64
+learning_rate=0.1
+
+tf.reset_default_graph()    
+
+X = tf.placeholder(tf.float32, [None, input_size], name="x")
+
+rbm = RBM(input_size,hidden_size,X,learning_rate) 
+
+
+op = [rbm.W_up, rbm.c_up, rbm.b_up]
+
+
+
+init = tf.global_variables_initializer()
+#tf.default_graph().finalized()
+
+
+with tf.Session() as sess:
+    
+    sess.run(init)
+    
+    for i in range(num_epochs):
+    
+        weights=sess.run(op,feed_dict = {X: mnist.train.next_batch(Batch_size)[0]})
+             
+        print ("iteration  ", i)   
+
+
+
+    def plotNNFilter(w):
+        filters = 64
+    #plt.figure(1, figsize=(20,20))
+        n_columns =8
+        n_rows = np.ceil(filters / n_columns) + 1
+        for i in range(64):
+            plt.figure(1)
+            plt.subplot(n_rows, n_columns, i+1)
+            plt.imshow(np.reshape(w[:,i], [28,28]), cmap="gray")        
+            
+    plotNNFilter (weights[0])
+    
+    
+    ##################### test ##################################################
+    sess = tf.Session()
+    a = mnist.train.next_batch(1)[0]
+    def sample(p):              
+        prop = tf.random_uniform(tf.shape(p), 0,1)
+        return tf.floor(prop + p) 
+         
+    h_prop = tf.nn.sigmoid(tf.matmul(a , weights[0]) + weights[2])
+    h_k = sample(h_prop)
+    
+    x_prop = tf.nn.sigmoid(tf.matmul(h_k , tf.transpose(weights[0])) + weights[1])
+    x_k = sample(x_prop)
+    plt.figure(2)
+
+    plt.imshow(np.reshape(a, [28,28]), cmap="gray")
+    
+    xx=sess.run(x_k)
+    plt.figure(3)
+    plt.imshow(np.reshape(xx, [28,28]), cmap="gray")
+    
